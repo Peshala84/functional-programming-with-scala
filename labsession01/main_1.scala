ca7f9ea94a07645cc22576f45d1d588b06b571cd
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-06-16 23:12:57 +0530

    first labsheet commit

---
+++ labsession01/main_1.scala
@@ -0,0 +1,10 @@
+object main_1 {  
+
+def  area_of_disk(radius: Double): Double = {
+    var pi=3.14159;
+    return pi*radius*radius;
+}
+var radius=5.0;
+var area=area_of_disk(radius);
+println(area);
+}
