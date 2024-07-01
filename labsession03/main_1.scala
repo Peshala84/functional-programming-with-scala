de9d03a721c73a5ed0000756b914ba2ac8e02e12
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-07-01 20:12:35 +0530

    add labsession 03 file

---
+++ labsession03/main_1.scala
@@ -0,0 +1,12 @@
+object que_01 {
+  def reverseString(string: String): String = {
+    if (string.isEmpty) ""
+    else reverseString(string.tail) + string.head
+  }
+
+  def main(args: Array[String]): Unit = {
+    var string = "String to be reversed"
+    var reversed_string = reverseString(string)
+    println(reversed_string)
+  }
+}
---
+++ labsession03/main_2.scala
@@ -0,0 +1,12 @@
+object que_02{
+
+    def filterAboveFive(list: List[String]):List[String]={
+        list.filter(_.length > 5)
+    }
+
+    def main(args: Array[String]):Unit={
+        var MyList = List("will print" , "no no" , "ofcause this" , "doNot" , "final one")
+        var aboveFive = filterAboveFive(MyList)
+        println(aboveFive)
+    }
+}
