b70c9e0f9f5852da6c4f4f1acd16629a1b5841c0
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-07-02 20:34:01 +0530

    update que_1

--- labsession03/main_1.scala
+++ labsession03/main_1.scala
@@ -5,7 +5,7 @@ object que_01 {
   }
 
   def main(args: Array[String]): Unit = {
-    var string = "String to be reversed"
+    var string = "Ramesh Peshala"
     var reversed_string = reverseString(string)
     println(reversed_string)
   }
