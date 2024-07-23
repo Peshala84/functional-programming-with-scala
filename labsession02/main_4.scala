77f69b2322dde48b86ed250d9d22232bb94311a1
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-07-23 09:40:09 +0530

    chore: Refactor main_4.scala for better readability and efficiency

--- labsession02/main_4.scala
+++ labsession02/main_4.scala
@@ -1,5 +1,4 @@
-object que_4{
-    
+object question04{
     def main(args: Array[String]): Unit={
         println(s"Ticket Price     Profit")
         println(s"5                ${profit(5)}")
@@ -7,26 +6,42 @@ object que_4{
         println(s"15               ${profit(15)}")
         println(s"20               ${profit(20)}")
         println(s"25               ${profit(25)}")
-
+        println(s"30               ${profit(30)}")
+        println ("\n")
+        println(s"Best profitable ticket price: ${bestPrice()}")
     }
 
-    def noOfAttendees(ticket_price: Double): Double={
-        120 + (15 - ticket_price) * 4 // /5 *20
+    def attendees(ticket_price:Double):Int={
+        120 + ((15 - ticket_price)/5 * 20).toInt
     }
 
-    def cost(ticket_price: Double): Double={
-        var noOfiter = (noOfAttendees(ticket_price) / 120).toInt 
-        if(noOfiter == 0){
-            noOfiter = 1
+    def revenue(ticket_price:Double)={
+        attendees(ticket_price) * ticket_price
+    }
+
+    def cost(ticket_price:Double)={
+        500 + 3*attendees(ticket_price)
+    }
+
+    def profit(ticket_price:Double)={
+        revenue(ticket_price) - cost(ticket_price)
+    }
+
+    def bestPrice(): Double = {
+        var ticket_price = 5.0
+        var maxProfit = profit(ticket_price)
+        var bestPrice = ticket_price
+
+        while (attendees(ticket_price) >= 0) {
+            val currentProfit = profit(ticket_price)
+            if (currentProfit > maxProfit) {
+                maxProfit = currentProfit
+                bestPrice = ticket_price
+            }
+            ticket_price += 5.0
         }
-        return noOfiter * 500 + noOfAttendees(ticket_price) * 3
+
+        bestPrice
     }
 
-    def income(ticket_price: Double): Double={
-        ticket_price * noOfAttendees(ticket_price)
-    }
-
-    def profit(ticket_price: Double): Double={
-        income(ticket_price) - cost(ticket_price)
-    }
 }
