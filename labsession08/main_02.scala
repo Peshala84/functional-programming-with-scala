3f43efc7e2f505e52b0d0ecf449ae64fa7198da7
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-08-12 21:49:33 +0530

    refactor: Add Caesar cipher encryption and decryption functions

---
+++ labsession08/main_02.scala
@@ -0,0 +1,26 @@
+import scala.io.StdIn
+
+object Multiples {
+
+    def main(arg : Array[String]): Unit ={
+
+        println("Enter the Number : ");
+        val Number = StdIn.readInt();
+
+        val isMultipleOf3 = (n: Int) => n % 3 == 0
+        val isMultipleOf5 = (n: Int) => n % 5 == 0
+
+        if (isMultipleOf3(Number) && isMultipleOf5(Number)) {
+            println("Multiple of Both Three and Five");
+        }
+        else if (isMultipleOf3(Number)) {
+            println("Multiple of Three");
+        }
+        else if (isMultipleOf5(Number)) {
+            println("Multiple of Five");
+        }
+        else {
+            println("Not a Multiple of Three or Five");
+        }
+    }
+}
