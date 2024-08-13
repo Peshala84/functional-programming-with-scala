61147f4028f94608e406a112af6ec19931a19f84
Ramesh Peshala <rameshpeshala84@gmail.com>
2024-08-13 07:30:52 +0530

    refactor: Update Caesar cipher implementation for better performance and readability

--- labsession08/main_1.scala
+++ labsession08/main_1.scala
@@ -1,18 +1,50 @@
-object CaesarCipher{
-    
-    def encrypt(text: String , shifter: Int): String = {
-        text.map(c => (c + shifter).toChar)
+object CaesarCiper {
+
+    def Encrypt(shift:Int,text:String): String ={
+    val newText = new StringBuilder
+    val normalizedShift = shift % 26 
+    for (i <- 0 until text.length) {
+      val value = text(i).toInt
+      val newChar = if (value >= 65 && value <= 90) { // Uppercase
+                        ((value - 65 + normalizedShift + 26) % 26 + 65).toChar
+                    }
+                    else if (value >= 97 && value <= 122) { // Lowercase
+                        ((value - 97 + normalizedShift + 26) % 26 + 97).toChar
+                    } else { // Non-alphabetic characters
+                        text(i)
+                    }
+      newText.append(newChar)
+    }
+    newText.toString()
     }
 
-    def decrypt(text: String , shifter: Int): String = {
-        text.map(c => (c - shifter).toChar)
+    def Decrypt(shift:Int,text:String): String = {
+    val normalizedShift = shift % 26 
+    val newText = new StringBuilder
+    for (i <- 0 until text.length) {
+      val value = text(i).toInt;
+      val newChar = if (value >= 65 && value <= 90) { // Uppercase
+        ((value - 65 - normalizedShift + 26) % 26 + 65).toChar
+      } else if (value >= 97 && value <= 122) { // Lowercase
+        ((value - 97 - normalizedShift + 26) % 26 + 97).toChar
+      } else { // Non-alphabetic characters
+        text(i)
+      }
+      newText.append(newChar)
+    }
+    newText.toString()
     }
 
-    def main(args: Array[String]): Unit={
-        val text = "Hello Scala"
-        println(s"Original Text : ${text}")
-        val encrypted_text = encrypt(text , 1) 
-        println(s"Encrypted Text : ${encrypted_text}")
-        println(s"Decrypted Text : ${decrypt(encrypted_text , 1)}")
-    }
+    def main(arg: Array[String]) : Unit = {
+        val shift = 3;
+        val text = "Ramesh";
+        println(s"The text is $text");
+        // String after encryption
+        val newString = Encrypt(shift,text); 
+        println(s"The text after encryption is $newString");
+        // String after decryption
+        val newString1 = Decrypt(shift,newString);
+        println(s"the text after decryption is $newString1");
+    } 
+
 }
